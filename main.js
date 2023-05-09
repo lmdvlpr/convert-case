15f5602bd4e517e30c733c03c87f618b346cb9e1
Lucas Mota <contato.lukamota@gmail.com>
2023-05-09 00:52:22 -0300

    initial commit

---
+++ main.js
@@ -0,0 +1,9 @@
+function converterParaMaiusculas() {
+  let texto = document.getElementById('texto')
+  texto.value = texto.value.toUpperCase()
+}
+
+function converterParaMinusculas() {
+  let texto = document.getElementById('texto')
+  texto.value = texto.value.toLowerCase()
+}
